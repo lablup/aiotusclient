15518983a0408ccb262aaaebe5e7d031814f9629
Joongi Kim <joongi@lablup.com>
2016-11-23 22:35:21 +0900

    Add "integration" mark to integration tests.
     * You may run only integration tests or exclude them using "-m"
       argument for pytest.
    
       e.g., python -m pytest -m 'not integration'

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,8 +5,25 @@ You should be running the API service on http://localhost:8081 to run this test
 suite.  Of course, the service must be fully configured as follows:
 
  - The gateway server must have at least one agent.
+
+   An example sequence to run both manager & agent locally:
+
+   docker run -e POSTGRES_DB=sorna \
+              -e POSTGRES_PASSWORD=develove \
+              -p 5432:5432 -d \
+              --name sorna-db \
+              postgres
+   python -m sorna.gateway.models \
+          --create-tables --populate-fixtures
+   docker run -p 6379:6379 -d \
+              --name sorna-redis \
+              redis
+   python -m sorna.agent.server --volume-root=`pwd`/volume-temp --max-kernels 3
+   python -m sorna.gateway.server --service-port=8081
+
  - The agent should have access to a Docker daemon and the
    "lablup/kernel-python3" docker image.
+
  - The gateway must have access to a test database that has pre-populated
    fixture data.
    (Check out `python -m sorna.gateway.models --populate-fixtures`)
@@ -22,46 +39,64 @@ from sorna.kernel import create_kernel, destroy_kernel, \
 from sorna.exceptions import SornaAPIError
 
 
+@pytest.mark.integration
 def test_connection(defconfig):
-    rqst = Request('GET', '/')
-    resp = rqst.send()
+    request = Request('GET', '/')
+    resp = request.send()
     assert 'version' in resp.json()
 
 
+@pytest.mark.integration
 @pytest.mark.asyncio
 async def test_async_connection(defconfig):
-    rqst = Request('GET', '/')
-    resp = await rqst.asend()
+    request = Request('GET', '/')
+    resp = await request.asend()
     assert 'version' in resp.json()
 
 
+@pytest.mark.integration
 def test_auth(defconfig):
     random_msg = uuid.uuid4().hex
-    rqst = Request('GET', '/authorize', {
+    request = Request('GET', '/authorize', {
         'echo': random_msg,
     })
-    rqst.sign()
-    resp = rqst.send()
+    request.sign()
+    resp = request.send()
     assert resp.status == 200
     data = resp.json()
     assert data['authorized'] == 'yes'
     assert data['echo'] == random_msg
 
 
+@pytest.mark.integration
+def test_auth_malformed(defconfig):
+    random_msg = uuid.uuid4().hex
+    request = Request('GET', '/authorize')
+    request.content = b'<this is not json>'
+    request.sign()
+    resp = request.send()
+    assert resp.status == 200
+    data = resp.json()
+    assert data['authorized'] == 'yes'
+    assert data['echo'] == random_msg
+
+
+@pytest.mark.integration
 @pytest.mark.asyncio
 async def test_async_auth(defconfig):
     random_msg = uuid.uuid4().hex
-    rqst = Request('GET', '/authorize', {
+    request = Request('GET', '/authorize', {
         'echo': random_msg,
     })
-    rqst.sign()
-    resp = await rqst.asend()
+    request.sign()
+    resp = await request.asend()
     assert resp.status == 200
     data = resp.json()
     assert data['authorized'] == 'yes'
     assert data['echo'] == random_msg
 
 
+@pytest.mark.integration
 def test_kernel_lifecycles(defconfig):
     kernel_id = create_kernel('python3')
     info = get_kernel_info(kernel_id)
@@ -76,6 +111,7 @@ def test_kernel_lifecycles(defconfig):
         assert e.status == 404
 
 
+@pytest.mark.integration
 def test_kernel_execution(defconfig):
     kernel_id = create_kernel('python3')
     result = execute_code(kernel_id, 'code-001', 'print("hello world")')
