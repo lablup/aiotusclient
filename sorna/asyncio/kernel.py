bb42ed95be480e155bdb3fd7c9731a3eb4df4e1a
Joongi Kim <joongi@lablup.com>
2016-12-23 11:47:22 +0900

    Add asyncio version of API wrapper functions.

---
+++ sorna/asyncio/kernel.py
@@ -0,0 +1,65 @@
+import uuid
+
+from ..exceptions import SornaAPIError
+from ..request import Request
+
+
+async def create_kernel(kernel_type, client_token=None, max_mem=0, timeout=0, return_id_only=True):
+    if client_token is not None:
+        assert isinstance(client_token, str)
+        assert len(client_token) > 8
+    request = Request('POST', '/kernel/create', {
+        'lang': kernel_type,
+        'clientSessionToken': client_token if client_token else uuid.uuid4().hex,
+        'resourceLimits': {
+            'maxMem': max_mem,
+            'timeout': timeout,
+        }
+    })
+    request.sign()
+    resp = await request.asend()
+    if resp.status == 201:
+        if return_id_only:
+            return resp.json()['kernelId']
+        return resp.json()
+    else:
+        raise SornaAPIError(resp.status, resp.reason, resp.text())
+
+
+async def destroy_kernel(kernel_id):
+    request = Request('DELETE', '/kernel/{}'.format(kernel_id))
+    request.sign()
+    resp = await request.asend()
+    if resp.status != 204:
+        raise SornaAPIError(resp.status, resp.reason, resp.text())
+
+
+async def restart_kernel(kernel_id):
+    request = Request('PATCH', '/kernel/{}'.format(kernel_id))
+    request.sign()
+    resp = await request.send()
+    if resp.status != 204:
+        raise SornaAPIError(resp.status, resp.reason, resp.text())
+
+
+async def get_kernel_info(kernel_id):
+    request = Request('GET', '/kernel/{}'.format(kernel_id))
+    request.sign()
+    resp = await request.asend()
+    if resp.status == 200:
+        return resp.json()
+    else:
+        raise SornaAPIError(resp.status, resp.reason, resp.text())
+
+
+async def execute_code(kernel_id, code_id, code):
+    request = Request('POST', '/kernel/{}'.format(kernel_id), {
+        'codeId': code_id,
+        'code': code,
+    })
+    request.sign()
+    resp = await request.asend()
+    if resp.status == 200:
+        return resp.json()['result']
+    else:
+        raise SornaAPIError(resp.status, resp.reason, resp.text())
