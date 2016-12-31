e0a648b95635aca21cb37ed183cd6e4d906e36f8
Jonghyun Park <adrysn@gmail.com>
2016-12-31 16:13:29 +0900

    Separate common tests elements

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 
 from sorna.exceptions import SornaAPIError
@@ -8,17 +10,35 @@ from sorna.kernel import (
 
 
 def test_create_kernel_url(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=201)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mocker.MagicMock(status=201,
+                                                      json=mock.MagicMock())
     mock_req = mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     create_kernel('python')
+
     mock_req.assert_called_once_with('POST', '/kernel/create', mocker.ANY)
+    mock_req_obj.sign.assert_called_once_with()
+    mock_req_obj.send.assert_called_once_with()
+    mock_req_obj.send.return_value.json.assert_called_once_with()
+
+
+def test_create_kernel_return_id_only(mocker):
+    mock_json_ftn = lambda: {'kernelId': 'mock_kernel_id'}
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(
+        status=201, json=mock_json_ftn
+    )
+    mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
+
+    resp = create_kernel('python')
+
+    assert resp == mock_json_ftn()['kernelId']
 
 
 def test_create_kernel_raises_err_with_abnormal_status(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=400)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(status=400)
     mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     with pytest.raises(SornaAPIError):
@@ -26,17 +46,20 @@ def test_create_kernel_raises_err_with_abnormal_status(mocker):
 
 
 def test_destroy_kernel_url(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=204)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(status=204)
     mock_req = mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     destroy_kernel(1)
+
     mock_req.assert_called_once_with('DELETE', '/kernel/{}'.format(1))
+    mock_req_obj.sign.assert_called_once_with()
+    mock_req_obj.send.assert_called_once_with()
 
 
 def test_destroy_kernel_raises_err_with_abnormal_status(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=400)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(status=400)
     mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     with pytest.raises(SornaAPIError):
@@ -44,17 +67,20 @@ def test_destroy_kernel_raises_err_with_abnormal_status(mocker):
 
 
 def test_restart_kernel_url(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=204)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(status=204)
     mock_req = mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     restart_kernel(1)
+
     mock_req.assert_called_once_with('PATCH', '/kernel/{}'.format(1))
+    mock_req_obj.sign.assert_called_once_with()
+    mock_req_obj.send.assert_called_once_with()
 
 
 def test_restart_kernel_raises_err_with_abnormal_status(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=400)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(status=400)
     mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     with pytest.raises(SornaAPIError):
@@ -62,17 +88,21 @@ def test_restart_kernel_raises_err_with_abnormal_status(mocker):
 
 
 def test_get_kernel_info_url(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=200)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(status=200)
     mock_req = mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     get_kernel_info(1)
+
     mock_req.assert_called_once_with('GET', '/kernel/{}'.format(1))
+    mock_req_obj.sign.assert_called_once_with()
+    mock_req_obj.send.assert_called_once_with()
+    mock_req_obj.send.return_value.json.assert_called_once_with()
 
 
 def test_get_kernel_info_raises_err_with_abnormal_status(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=400)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(status=400)
     mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     with pytest.raises(SornaAPIError):
@@ -80,18 +110,22 @@ def test_get_kernel_info_raises_err_with_abnormal_status(mocker):
 
 
 def test_execute_code_url(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=200)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(status=200)
     mock_req = mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     execute_code(1, 2, 'hello')
+
     mock_req.assert_called_once_with('POST', '/kernel/{}'.format(1),
                                      {'codeId': 2, 'code': 'hello'})
+    mock_req_obj.sign.assert_called_once_with()
+    mock_req_obj.send.assert_called_once_with()
+    mock_req_obj.send.return_value.json.assert_called_once_with()
 
 
 def test_execute_code_raises_err_with_abnormal_status(mocker):
-    mock_req_obj = mocker.Mock()
-    mock_req_obj.send.return_value = mocker.MagicMock(status=400)
+    mock_req_obj = mock.Mock()
+    mock_req_obj.send.return_value = mock.MagicMock(status=400)
     mocker.patch('sorna.kernel.Request', return_value=mock_req_obj)
 
     with pytest.raises(SornaAPIError):
--- tests/test_request.py
+++ tests/test_request.py
@@ -8,6 +8,7 @@ import pytest
 import requests
 import simplejson as json
 
+from .common import mock_coro, MockAsyncContextManager
 from sorna.exceptions import SornaAPIError
 from sorna.request import Request, Response
 
@@ -49,39 +50,6 @@ def mock_sorna_aresp():
     }
 
     return MockAsyncContextManager(**conf), conf
-
-
-def mock_coro(return_value):
-    """
-    Return mock coroutine.
-    Python's default mock module does not support coroutines.
-    """
-    async def mock_coro(*args, **kargs):
-        return return_value
-    return mock.Mock(wraps=mock_coro)
-
-
-class MockAsyncContextManager:
-    """
-    Mock async context manager.
-    To get around `async with` statement for testing.
-    Attributes can be set by passing `kwargs`.
-    """
-    def __init__(self, *args, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-
-    def __enter__(self):
-        pass
-
-    def __exit__(self, exc_type, exc_value, exc_tb):
-        pass
-
-    async def __aenter__(self):
-        pass
-
-    async def __aexit__(self, exc_type, exc_value, exc_tb):
-        pass
 
 
 def test_request_initialization(req_params):
