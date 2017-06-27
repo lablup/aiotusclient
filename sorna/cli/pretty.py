4636981f31554a12e93aa49b2ad8cf7f388a713d
Joongi Kim <joongi@lablup.com>
2017-06-27 16:51:38 +0900

    refs #4: Prettify "run" command output

---
+++ sorna/cli/pretty.py
@@ -0,0 +1,44 @@
+import enum
+import functools
+import sys
+import time
+
+
+class PrintStatus(enum.Enum):
+    NONE = 0
+    WAITING = 1
+    DONE = 2
+    FAILED = 3
+
+
+_last_width = 1
+_last_printed = False
+
+
+def print_pretty(msg, *, status=PrintStatus.NONE, file=sys.stderr):
+    global _last_width, _last_printed, _color_init
+    if status == PrintStatus.NONE:
+        indicator = '\x1b[96m\u2219' if file.isatty() else '\u2219'
+    elif status == PrintStatus.WAITING:
+        indicator = '\x1b[93m\u22EF' if file.isatty() else '\u22EF'
+    elif status == PrintStatus.DONE:
+        indicator = '\x1b[92m\u2714' if file.isatty() else '\u2714'
+    elif status == PrintStatus.FAILED:
+        indicator = '\x1b[91m\u2718' if file.isatty() else '\u2718'
+    else:
+        raise ValueError
+    print('{0}\r'.format(' ' * _last_width), end='', file=file)
+    text = '{0} {1}'.format(indicator, msg)
+    if file.isatty():
+        text += '\x1b[0m'
+    _last_width = len(text) or 1
+    print('{0}\r'.format(text), end='', file=file)
+    file.flush()
+    if status != PrintStatus.WAITING:
+        print('', file=file)
+
+
+print_none = functools.partial(print_pretty, status=PrintStatus.NONE)
+print_wait = functools.partial(print_pretty, status=PrintStatus.WAITING)
+print_done = functools.partial(print_pretty, status=PrintStatus.DONE)
+print_fail = functools.partial(print_pretty, status=PrintStatus.FAILED)
--- sorna/cli/run.py
+++ sorna/cli/run.py
@@ -1,7 +1,9 @@
-import sys
 import getpass
+import sys
+import traceback
 
 from . import register_command
+from .pretty import print_none, print_wait, print_done, print_fail
 from ..kernel import Kernel
 from ..compat import token_hex
 
@@ -10,59 +12,75 @@ from ..compat import token_hex
 def run(args):
     '''Run the code.'''
     attach_to_existing = True
-    vprint = print if args.verbose else lambda *args, **kwargs: None
+    if args.verbose:
+        vprint_none = print_none
+        vprint_wait = print_wait
+        vprint_done = print_done
+        vprint_fail = print_fail
+    else:
+        vprint_none = vprint_wait = lambda *args, **kwargs: None
+        vprint_done = vprint_fail = lambda *args, **kwargs: None
     if not args.client_token:
         args.client_token = token_hex(16)
         attach_to_existing = False
-        vprint('Creating temporary kernel '
-               '(client-token: {0})'.format(args.client_token))
-    kernel = Kernel.get_or_create(args.lang, args.client_token)
-    vprint('Connected to kernel (id: {0})'.format(kernel.kernel_id))
-
-    if args.files:
-        if args.code:
-            print('You can run only either source files or command-line '
-                  'code snippet.', file=sys.stderr)
-            return
-        # upload files
-        ret = kernel.upload(args.files)
-        assert ret.status // 100 == 2
-        # run code
-        vprint('Upload done.')
-        #result = kernel.execute(mode='batch')
-        #print(result)
+        vprint_none('Client session token: {0}'.format(args.client_token))
+        vprint_wait('Creating a temporary kernel...')
     else:
-        if not args.code:
-            print('You should provide the command-line code snippet using '
-                  '"-c" option if run without files.', file=sys.stderr)
-            return
-        # run code
-        while True:
-            result = kernel.execute(args.code, mode='query')
-            for rec in result['console']:
-                if rec[0] == 'stdout':
-                    print(rec[1], end='', file=sys.stdout)
-                elif rec[0] == 'stderr':
-                    print(rec[1], end='', file=sys.stderr)
-                else:
-                    print('----- output record (type: {0}) -----'.format(rec[0]))
-                    print(rec[1])
-                    print('----- end of record -----')
-            sys.stdout.flush()
-            if result['status'] == 'finished':
-                break
-            elif result['status'] == 'waiting-input':
-                if result['options'].get('is_password', False):
-                    args.code = getpass.getpass()
-                else:
-                    args.code = input()
-            elif result['status'] == 'continued':
-                continue
+        vprint_none('Client session token: {0}'.format(args.client_token))
+        vprint_wait('Connecting to the kernel...')
+    kernel = Kernel.get_or_create(args.lang, args.client_token)
+    vprint_done('Kernel (ID: {0}) is ready.'.format(kernel.kernel_id))
 
-    if not attach_to_existing:
-        kernel.destroy()
-        vprint('Destroyed kernel '
-               '(client-token: {0}).'.format(args.client_token))
+    try:
+        if args.files:
+            if args.code:
+                print('You can run only either source files or command-line '
+                      'code snippet.', file=sys.stderr)
+                return
+            vprint_wait('Uploading source files...')
+            ret = kernel.upload(args.files)
+            if ret.status // 100 != 2:
+                print_fail('Uploading source files failed!')
+                print('{0}: {1}\n{2}'.format(ret.status, ret.reason, ret.text()))
+                return
+            vprint_done('Uploading done.')
+            #result = kernel.execute(mode='batch')
+            #print(result)
+        else:
+            if not args.code:
+                print('You should provide the command-line code snippet using '
+                      '"-c" option if run without files.', file=sys.stderr)
+                return
+            # run code
+            while True:
+                result = kernel.execute(args.code, mode='query')
+                for rec in result['console']:
+                    if rec[0] == 'stdout':
+                        print(rec[1], end='', file=sys.stdout)
+                    elif rec[0] == 'stderr':
+                        print(rec[1], end='', file=sys.stderr)
+                    else:
+                        print('----- output record (type: {0}) -----'.format(rec[0]))
+                        print(rec[1])
+                        print('----- end of record -----')
+                sys.stdout.flush()
+                if result['status'] == 'finished':
+                    break
+                elif result['status'] == 'waiting-input':
+                    if result['options'].get('is_password', False):
+                        args.code = getpass.getpass()
+                    else:
+                        args.code = input()
+                elif result['status'] == 'continued':
+                    continue
+    except:
+        print_fail('Execution failed!')
+        traceback.print_exc()
+    finally:
+        if not attach_to_existing:
+            vprint_wait('Cleaning up the temporary kernel...')
+            kernel.destroy()
+            vprint_done('Cleaned up the kernel.')
 
 
 run.add_argument('lang',
