2c8f7e97235b17e09f42ad1237e5cc2491205c08
Joongi Kim <joongi@lablup.com>
2017-10-18 12:08:47 +0900

    ci: Fix some typo....

--- ai/backend/client/cli/config.py
+++ ai/backend/client/cli/config.py
@@ -11,6 +11,6 @@ def config(args):
     print('API endpoint: "{0}"'.format(config.endpoint))
     print('API version: "{0}"'.format(config.version))
     print('Access key: "{0}"'.format(config.access_key))
-    masked_skey = config.secret_key[:6] + ('*' * 24) + config.secret_key[-10:])
-    print('Secret key: "{0}"'.format(masekd_skey)
+    masked_skey = config.secret_key[:6] + ('*' * 24) + config.secret_key[-10:]
+    print('Secret key: "{0}"'.format(masked_skey))
     print('Signature hash type: "{0}"'.format(config.hash_type))
