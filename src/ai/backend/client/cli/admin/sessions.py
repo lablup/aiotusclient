1ebae565210ce7a32033cad261e6f8b07a75b330
tink-expo <whhan8485@gmail.com>
2019-02-02 22:06:37 +0900

    cli admin sessions num-of-sessions option, gql pagination

--- src/ai/backend/client/cli/admin/sessions.py
+++ src/ai/backend/client/cli/admin/sessions.py
@@ -1,4 +1,5 @@
 import sys
+import asyncio
 
 import click
 from tabulate import tabulate
@@ -6,6 +7,7 @@ from tabulate import tabulate
 from . import admin
 from ...session import Session, is_legacy_server
 from ..pretty import print_error
+from ...compat import current_loop
 
 
 @admin.command()
@@ -18,10 +20,47 @@ from ..pretty import print_error
               help='Get sessions for a specific access key '
                    '(only works if you are a super-admin)')
 @click.option('--id-only', is_flag=True, help='Display session ids only.')
-def sessions(status, access_key, id_only):
+@click.option('--num-of-sessions', type=int, default=None,
+              help='number of sessions wanted.')
+def sessions(status, access_key, id_only, num_of_sessions):
     '''
     List and manage compute sessions.
     '''
+    async def execute_paginated_query(session, fields, limit, offset):
+        q = 'query($limit:Int!, $offset:Int!, $ak:String, $status:String) {' \
+            '  compute_sessions(limit:$limit, offset:$offset, access_key:$ak, status:$status) { $fields }' \
+            '}'
+        q = q.replace('$fields', ' '.join(item[1] for item in fields))
+        v = {
+            'limit': limit,
+            'offset': offset,
+            'status': status if status != 'ALL' else None,
+            'ak': access_key,
+        }
+        try:
+            resp = session.Admin.query(q, v)
+        except Exception as e:
+            print_error(e)
+            sys.exit(1)
+        return resp['compute_sessions']
+        
+    async def collect_queries(session, fields, required_count, paginating_limit):
+        tasks = []
+        offset = 0
+        while offset < required_count:
+            limit = min(paginating_limit, required_count - offset)
+            tasks.append(asyncio.ensure_future(execute_paginated_query(session, fields, limit, offset)))
+            offset += paginating_limit
+        try:
+            responses = await asyncio.gather(*tasks, return_exceptions=True)
+        except Exception as e:
+            print_error(e)
+            sys.exit(1)
+        results = []
+        for res in responses:
+            results += res
+        return results
+
     fields = [
         ('Session ID', 'sess_id'),
     ]
@@ -39,41 +78,46 @@ def sessions(status, access_key, id_only):
         if is_legacy_server():
             del fields[2]
 
-    if access_key is None:
-        q = 'query($status:String) {' \
-            '  compute_sessions(status:$status) { $fields }' \
-            '}'
-    else:
-        q = 'query($ak:String, $status:String) {' \
-            '  compute_sessions(access_key:$ak, status:$status) { $fields }' \
-            '}'
-    q = q.replace('$fields', ' '.join(item[1] for item in fields))
+    # get count of sessions
+    q = 'query($ak:String, $status:String) {' \
+        '  count_compute_sessions(access_key:$ak, status:$status) { count }' \
+        '}'
     v = {
         'status': status if status != 'ALL' else None,
         'ak': access_key,
     }
+
     with Session() as session:
         try:
             resp = session.Admin.query(q, v)
         except Exception as e:
             print_error(e)
             sys.exit(1)
-        if len(resp['compute_sessions']) == 0:
+        total_count = resp['count_compute_sessions']['count']
+
+        required_count = min(total_count, num_of_sessions) if num_of_sessions else total_count
+        paginating_limit = 4
+
+        loop = asyncio.get_event_loop()
+        results = loop.run_until_complete(collect_queries(session, fields, required_count, paginating_limit))
+        loop.close()
+
+        if len(results) == 0:
             print('There are no compute sessions currently running.')
             return
-        for item in resp['compute_sessions']:
+        for item in results:
             if 'mem_cur_bytes' in item:
                 item['mem_cur_bytes'] = round(item['mem_cur_bytes'] / 2 ** 20, 1)
             if 'mem_max_bytes' in item:
                 item['mem_max_bytes'] = round(item['mem_max_bytes'] / 2 ** 20, 1)
         fields = [field for field in fields if field[1]
-                  in resp['compute_sessions'][0]]
+                  in results[0]]
 
         if id_only:
-            for item in resp['compute_sessions']:
+            for item in results:
                 print(item['sess_id'])
         else:
-            print(tabulate((item.values() for item in resp['compute_sessions']),
+            print(tabulate((item.values() for item in results),
                            headers=(item[0] for item in fields)))
 
 
