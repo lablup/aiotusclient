ef93c88ea4c4afed3f3e1af78a18fdb31d572fae
Joongi Kim <joongi@lablup.com>
2019-05-10 23:22:52 -0700

    ci: Stabilize environment-specific tests for CLI commands

--- tests/cli/test_cli_commands.py
+++ tests/cli/test_cli_commands.py
@@ -5,8 +5,7 @@ import pytest
 from click.testing import CliRunner
 
 from ai.backend.client.cli import main
-from ai.backend.client.config import get_config
-# from ai.backend.client.cli.run import run
+from ai.backend.client.config import get_config, set_config
 
 
 @pytest.fixture(scope="module")
@@ -27,7 +26,14 @@ def test_print_help_for_unknown_command(runner):
     assert re.match(r'Usage: (\w+) \[OPTIONS\] COMMAND \[ARGS\]', result.output)
 
 
-def test_config(runner):
+def test_config(runner, monkeypatch, example_keypair,
+                unused_tcp_port_factory):
+    api_port = unused_tcp_port_factory()
+    api_url = 'http://127.0.0.1:{}'.format(api_port)
+    set_config(None)
+    monkeypatch.setenv('BACKEND_ACCESS_KEY', example_keypair[0])
+    monkeypatch.setenv('BACKEND_SECRET_KEY', example_keypair[1])
+    monkeypatch.setenv('BACKEND_ENDPOINT', api_url)
     config = get_config()
     result = runner.invoke(main, ['config'])
     assert result.exit_code == 0
@@ -36,6 +42,14 @@ def test_config(runner):
     assert config.access_key in result.output
     assert config.secret_key[:6] in result.output
     assert config.hash_type in result.output
+
+
+def test_config_unset(runner, monkeypatch):
+    monkeypatch.delenv('BACKEND_ACCESS_KEY', raising=False)
+    monkeypatch.delenv('BACKEND_SECRET_KEY', raising=False)
+    monkeypatch.delenv('BACKEND_ENDPOINT', raising=False)
+    result = runner.invoke(main, ['config'])
+    assert result.exit_code == 1
 
 
 def test_compiler_shortcut(mocker):
@@ -54,7 +68,13 @@ def test_compiler_shortcut(mocker):
     assert sys.argv == ['lpython', '-h']
 
 
-def test_run_file_or_code_required(runner):
+def test_run_file_or_code_required(runner, monkeypatch, example_keypair,
+                                   unused_tcp_port_factory):
+    api_port = unused_tcp_port_factory()
+    api_url = 'http://127.0.0.1:{}'.format(api_port)
+    monkeypatch.setenv('BACKEND_ACCESS_KEY', example_keypair[0])
+    monkeypatch.setenv('BACKEND_SECRET_KEY', example_keypair[1])
+    monkeypatch.setenv('BACKEND_ENDPOINT', api_url)
     result = runner.invoke(main, ['run', 'python'])
     assert result.exit_code == 1
     assert 'provide the command-line code snippet' in result.output
