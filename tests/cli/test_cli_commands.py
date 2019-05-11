208f6cd0f2f942374488c8086c015c97d6ab014a
Joongi Kim <joongi@lablup.com>
2019-05-10 22:26:26 -0700

    ci: Update tests/cli/test_cli_commands to use Click's test utils

--- tests/cli/test_cli_commands.py
+++ tests/cli/test_cli_commands.py
@@ -1,37 +1,41 @@
 import sys
+import re
 
 import pytest
+from click.testing import CliRunner
 
 from ai.backend.client.cli import main
 from ai.backend.client.config import get_config
 # from ai.backend.client.cli.run import run
 
 
-@pytest.mark.parametrize('cmd', ['-h', '--help', 'help'])
-def test_print_help(cmd, capsys, mocker):
-    mocker.patch.object(sys, 'argv', ['backend.ai', cmd])
-    try:
-        main()
-    except SystemExit:
-        pass
-    out, err = capsys.readouterr()
-    assert 'Usage: backend.ai' in out or \
-           'Usage: backend.ai' in err
+@pytest.fixture(scope="module")
+def runner():
+    return CliRunner()
 
 
-def test_config(capsys, mocker):
-    mocker.patch.object(sys, 'argv', ['backend.ai', 'config'])
+@pytest.mark.parametrize('help_arg', ['-h', '--help'])
+def test_print_help(runner, help_arg):
+    result = runner.invoke(main, [help_arg])
+    assert result.exit_code == 0
+    assert re.match(r'Usage: (\w+) \[OPTIONS\] COMMAND \[ARGS\]', result.output)
+
+
+def test_print_help_for_unknown_command(runner):
+    result = runner.invoke(main, ['x-non-existent-command'])
+    assert result.exit_code == 2
+    assert re.match(r'Usage: (\w+) \[OPTIONS\] COMMAND \[ARGS\]', result.output)
+
+
+def test_config(runner):
     config = get_config()
-    try:
-        main()
-    except SystemExit:
-        pass
-    out, _ = map(str, capsys.readouterr())
-    assert str(config.endpoint) in out
-    assert config.version in out
-    assert config.access_key in out
-    assert config.secret_key[:6] in out
-    assert config.hash_type in out
+    result = runner.invoke(main, ['config'])
+    assert result.exit_code == 0
+    assert str(config.endpoint) in result.output
+    assert config.version in result.output
+    assert config.access_key in result.output
+    assert config.secret_key[:6] in result.output
+    assert config.hash_type in result.output
 
 
 def test_compiler_shortcut(mocker):
@@ -50,12 +54,7 @@ def test_compiler_shortcut(mocker):
     assert sys.argv == ['lpython', '-h']
 
 
-class TestRunCommand:
-    def test_either_code_or_file_is_required(self, capsys, mocker):
-        mocker.patch.object(sys, 'argv', ['backend.ai', 'run', 'python'])
-        try:
-            main()
-        except SystemExit:
-            pass
-        _, err = capsys.readouterr()
-        assert 'provide the command-line code snippet using "-c"' in err
+def test_run_file_or_code_required(runner):
+    result = runner.invoke(main, ['run', 'python'])
+    assert result.exit_code == 1
+    assert 'provide the command-line code snippet' in result.output
